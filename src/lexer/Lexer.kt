d2b26eb33dae490547b1ca4681e89a8e8728c390
NachoEstevo <ignacio.estevo@ing.austral.edu.ar>
2024-03-11 20:09:59 -0300

    implemented lexerInterface

--- src/lexer/Lexer.kt
+++ src/lexer/Lexer.kt
@@ -3,8 +3,8 @@ package lexer
 import Token.Token
 import lexer.generatorTree.GeneratorTokenTree
 
-class Lexer(private val tokenGenerators: List<GeneratorTokenTree>) {
-    fun lex(input: String): List<Token> {
+class Lexer(private val tokenGenerators: List<GeneratorTokenTree>) : LexerInterface {
+    override fun lex(input: String): List<Token> {
 
         return emptyList()
     }
