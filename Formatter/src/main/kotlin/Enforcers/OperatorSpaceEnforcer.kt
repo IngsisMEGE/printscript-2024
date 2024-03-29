132c0f5fbfcc800bbd65a32167fbaa43bfd1ec38
gonzamato <gonzalo.mato@ing.austral.edu.ar>
2024-03-29 13:44:40 -0300

    enfocer Operation Space ready

---
+++ Formatter/src/main/kotlin/Enforcers/OperatorSpaceEnforcer.kt
@@ -0,0 +1,36 @@
+package Enforcers
+
+class OperatorSpaceEnforcer() : Enforcer {
+    override fun enforceRule(code: String): String {
+        val modifiedCode = StringBuilder()
+        var insideString = false
+        var escaped = false
+
+        for (char in code) {
+            if (char == '\"' && !escaped) {
+                insideString = !insideString
+            }
+            if (char == '\\') {
+                escaped = !escaped
+            } else {
+                escaped = false
+            }
+
+            if (!insideString){
+                if (char == '+' || char == '-' || char == '*' || char == '/' || char == '%') {
+                    modifiedCode.append(" ")
+                    modifiedCode.append(char)
+                    modifiedCode.append(" ")
+                } else {
+                    modifiedCode.append(char)
+            }
+            } else {
+                modifiedCode.append(char)
+            }
+        }
+
+        return modifiedCode.toString()
+    }
+
+
+}
