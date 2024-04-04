fad80f4a79aea44f00569bfcc8107d9f2109eeb7
NachoEstevo <ignacio.estevo@ing.austral.edu.ar>
2024-04-04 00:35:30 -0300

    Update File when formatted

--- CLI/src/main/kotlin/Cli.kt
+++ CLI/src/main/kotlin/Cli.kt
@@ -1,5 +1,6 @@
 package org.example
 
+import java.io.File
 import java.io.FileNotFoundException
 import java.lang.Exception
 
@@ -44,19 +45,21 @@ fun main() {
                 }
             }
 
-            "2" -> {
-                println("Please enter the file path:")
-                val filePath = readlnOrNull()
-                try {
-                    if (filePath != null) {
-                        println(cli.printScript.format(filePath))
-                    }
-                } catch (e: FileNotFoundException) {
-                    println("File not found: $filePath")
-                } catch (e: Exception) {
-                    println("An error occurred: ${e.message}")
-                }
-            }
+           "2" -> {
+    println("Please enter the file path:")
+    val filePath = readlnOrNull()
+    try {
+        if (filePath != null) {
+            val formattedContent = cli.printScript.format(filePath)
+            File(filePath).writeText(formattedContent)
+            println("File updated successfully.")
+        }
+    } catch (e: FileNotFoundException) {
+        println("File not found: $filePath")
+    } catch (e: Exception) {
+        println("An error occurred: ${e.message}")
+    }
+}
 
             "3" -> {
                 println("Please enter the configuration filepath:")
