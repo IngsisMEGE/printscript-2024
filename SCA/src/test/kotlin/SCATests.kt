c8a904788f7663d58dd1d0e214ec574ba9bc1c8d
Juan Cruz Estevez <juanxestevez@gmail.com>
2024-04-08 18:45:18 -0300

    SCA

---
+++ SCA/src/test/kotlin/SCATests.kt
@@ -0,0 +1,93 @@
+import analyzers.RegularSCA
+import astn.Method
+import astn.OperationHead
+import astn.OperationString
+import astn.VarDeclaration
+import token.DataType
+import token.Token
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SCATests {
+    @Test
+    fun camelCaseTest() {
+        val rules = mapOf("CamelCaseFormat" to true)
+        val sca = RegularSCA(rules)
+        val declaration =
+            VarDeclaration(
+                Token(DataType.NUMBER_TYPE, "number", Pair(4, 0), Pair(5, 0)),
+                Token(DataType.VARIABLE_NAME, "snake_case", Pair(0, 0), Pair(1, 0)),
+            )
+        val response = sca.readAst(declaration)
+        assertEquals("Invalid typing format in line 0 row 0", response)
+    }
+
+    @Test
+    fun camelCaseTest2() {
+        val rules = mapOf("CamelCaseFormat" to true)
+        val sca = RegularSCA(rules)
+        val declaration =
+            VarDeclaration(
+                Token(DataType.NUMBER_TYPE, "number", Pair(4, 0), Pair(5, 0)),
+                Token(DataType.VARIABLE_NAME, "camelCase", Pair(0, 0), Pair(1, 0)),
+            )
+        val response = sca.readAst(declaration)
+        assertEquals("", response)
+    }
+
+    @Test
+    fun snake_caseTest() {
+        val rules = mapOf("SnakeCaseFormat" to true)
+        val sca = RegularSCA(rules)
+        val declaration =
+            VarDeclaration(
+                Token(DataType.NUMBER_TYPE, "number", Pair(4, 0), Pair(5, 0)),
+                Token(DataType.VARIABLE_NAME, "camelCase", Pair(0, 0), Pair(1, 0)),
+            )
+        val response = sca.readAst(declaration)
+        assertEquals("Invalid typing format in line 0 row 0", response)
+    }
+
+    @Test
+    fun snake_caseTest2() {
+        val rules = mapOf("SnakeCaseFormat" to true)
+        val sca = RegularSCA(rules)
+        val declaration =
+            VarDeclaration(
+                Token(DataType.NUMBER_TYPE, "number", Pair(4, 0), Pair(5, 0)),
+                Token(DataType.VARIABLE_NAME, "snake_case", Pair(0, 0), Pair(1, 0)),
+            )
+        val response = sca.readAst(declaration)
+        assertEquals("", response)
+    }
+
+    @Test
+    fun methodCaseTest() {
+        val rules = mapOf("MethodNoExpresion" to true)
+        val sca = RegularSCA(rules)
+        val method =
+            Method(
+                Token(DataType.VARIABLE_NAME, "IAMNOTAMETHOD", Pair(0, 0), Pair(6, 0)),
+                OperationString(Token(DataType.STRING_VALUE, "Hello", Pair(7, 0), Pair(12, 0))),
+            )
+        val response = sca.readAst(method)
+        assertEquals("", response)
+    }
+
+    @Test
+    fun methodCaseTest2() {
+        val rules = mapOf("MethodNoExpresion" to true)
+        val sca = RegularSCA(rules)
+        val method =
+            Method(
+                Token(DataType.VARIABLE_NAME, "IAMNOTAMETHOD", Pair(0, 0), Pair(6, 0)),
+                OperationHead(
+                    Token(DataType.OPERATOR_PLUS, "+", Pair(0, 0), Pair(1, 0)),
+                    OperationString(Token(DataType.STRING_VALUE, "hello ", Pair(0, 0), Pair(1, 0))),
+                    OperationString(Token(DataType.STRING_VALUE, "world", Pair(0, 0), Pair(1, 0))),
+                ),
+            )
+        val response = sca.readAst(method)
+        assertEquals("Invalid operation in line 0, row 0", response)
+    }
+}
