52a24da19cc06ca799c48185c90797cf1937b9f5
Ignacio Estevo <ignacio.estevo@ing.austral.edu.ar>
2024-04-11 21:08:14 -0300

    Update AssignationExecution.kt

--- Interpreter/src/main/kotlin/Executors/AssignationExecution.kt
+++ Interpreter/src/main/kotlin/Executors/AssignationExecution.kt
@@ -3,18 +3,6 @@ package interpreter.executors
 import astn.Assignation
 import interpreter.Value
 
-/**
- * This class is responsible for executing variable assignations in the PrintScript application.
- * It implements the Executor interface and overrides the execute function.
- *
- * The execute function takes an Assignation AST and a map of variables as input. It retrieves the variable name and type from the AST,
- * and checks if the variable already exists in the map. If it does, it evaluates the value of the variable using a BinaryOperatorReader and checks if the evaluated value's type matches the declared type.
- * If the types match, it updates the variable in the map and returns an empty string. If the types do not match or if the variable does not exist, it throws an Exception.
- *
- * The getValueType function takes a DataType as input and returns the corresponding VariableType. It uses a when expression to determine the VariableType.
- *
- * @throws Exception If the variable does not exist, if the declared type does not match the evaluated value's type, or if the DataType is not NUMBER_TYPE or STRING_TYPE.
- */
 class AssignationExecution : Executor<Assignation> {
     private val binaryOperator = BinaryOperatorReader()
 
@@ -30,9 +18,9 @@ class AssignationExecution : Executor<Assignation> {
                 variables[varName] = value
                 return ""
             }
-            throw Exception("Variable type mismatch")
+            throw Exception("Variable type mismatch at Line ${ast.assignation.getInitialPosition().second}")
         } else {
-            throw Exception("Variable not found")
+            throw Exception("Variable not found at Line ${ast.assignation.getInitialPosition().second}")
         }
     }
 }
