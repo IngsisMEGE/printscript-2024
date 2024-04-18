245e04ef221cdeb38c5a5c191e10d415f0b6f3fe
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-04-18 01:34:23 -0300

    readInput is empty should work

--- parser/src/main/kotlin/astBuilders/OperationInputBuilder.kt
+++ parser/src/main/kotlin/astBuilders/OperationInputBuilder.kt
@@ -1,6 +1,7 @@
 package astBuilders
 
 import astn.OperationInput
+import astn.OperationString
 import token.DataType
 import token.Token
 
@@ -11,6 +12,13 @@ class OperationInputBuilder {
     }
 
     fun createInputOperation(tokens: List<Token>): OperationInput {
+        if (tokens.subList(
+                2,
+                tokens.size - 1,
+            ).isEmpty()
+        ) {
+            return OperationInput(OperationString(Token(DataType.STRING_VALUE, "", Pair(0, 0), Pair(0, 0))))
+        }
         return OperationInput(OperationBuilder().buildOperation(tokens.subList(2, tokens.size - 1)))
     }
 }
